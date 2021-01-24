0c79afea83a28c32de2f6d140744316505373e0b
zanalytics <chrispen@zanalytics.io>
2021-01-24 12:32:09 +0000

    added code to extract zip data

--- src/extract_transform/extract.py
+++ src/extract_transform/extract.py
@@ -1,40 +1,21 @@
 import requests
+from extract_functions import raw_data, download_zip
 
-
-def raw_data(url, destination):
-    """
-        Read an open csv through http. and write a specified location.
-
-        Parameters:
-            - url : string
-                HTTP url ending with the .csv extension.
-            - destination : string
-                Local path to write csv.
-
-        Returns:
-            - writes the file to destination.
-    """
-
-    r = requests.get(url, stream=True)
-
-    with open(destination, "wb") as csv:
-        for chunk in r.iter_content(chunk_size=10 ** 6):
-            if chunk:
-                csv.write(chunk)
-    return print("File written to destination")
-
-
-# Read and write the price paid data
+# Extract the price paid data
 price_paid_url = '''http://prod.publicdata.landregistry.gov.uk.s3-website-eu-
                     west-1.amazonaws.com/pp-complete.csv'''
-price_paid_dest = "./data/raw/pp-complete.csv"
-raw_data(price_paid_url, price_paid_dest)
+price_paid_dest = "./data/raw/"
+pp_file_name = "pp-complete.csv"
+raw_data(price_paid_url, price_paid_dest, pp_file_name)
 
-# Read and write the HPI data
+# Extract the HPI data
 hpi_url = '''http://publicdata.landregistry.gov.uk/market-trend-data/house-
                     price-index-data/Average-prices-Property-Type-2020-08.csv?
                     utm_medium=GOV.UK&utm_source=datadownload&utm_campaign=
                     average_price_property_price&utm_term=9.30_21_10_20'''
-hpi_dest = "./data/raw/house_price_index.csv"
+hpi_dest = "./data/raw/"
+hpi_file_name = "house_price_index.csv"
+raw_data(hpi_url, hpi_dest, hpi_file_name)
 
-raw_data(hpi_url, hpi_dest)
+# Extract the Postcodes data
+download_zip("https://www.doogal.co.uk/files/postcodes.zip", "./data/raw/")
