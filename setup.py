8f605ac2b5d8660590946c1cbf60c683996b0b61
zanalytics <chrispen@zanalytics.io>
2021-03-19 23:25:16 +0000

    empty new structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Price Paid Data - Analysis',
-    author='zanalytics',
-    license='MIT',
+    description='This is the Zanalytics housing project',
+    author='Zanalytics',
+    license='',
 )
